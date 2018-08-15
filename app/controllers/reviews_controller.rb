a438b02e52deda621f96371b7ae8152231eea520
Matthieu Rousseau <matthieurousseau86@gmail.com>
2018-08-15 15:32:16 +0200

    Completed

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to restaurant_path(params[:restaurant_id]), notice: 'Review was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Epicure',
+    address:      '148 av Gambetta 75020 Paris',
+    phone_number: '01 42 42 42 42',
+    category:     'french'
+  },
+  {
+    name:         'Suchic',
+    address:      '6 rue Vavin 75006',
+    phone_number: '01 42 42 42 42',
+    category:     'japanese'
+  },
+  {
+    name:         'Moulfrite',
+    address:      '200 bd St Germain 75005 Paris',
+    phone_number: '01 42 42 42 42',
+    category:     'belgian'
+  },
+  {
+    name:         'Chien et Chat',
+    address:      '7 rue de Belleville 75011 Paris',
+    phone_number: '01 42 42 42 42',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '01 53 42 42 42',
+    category:     'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
